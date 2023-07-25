deca65196b2a2d1cfdbb36ae76d7cef1203bcad8
Mohamed Jamal <mohamed.alali6784@gmail.com>
2023-07-25 17:39:58 +0400

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Mohamed Jamal AlAli on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
