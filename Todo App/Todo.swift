f7d0628f0d96d110edaf7c7d8d63f195cf48575a
Mohamed Jamal <mohamed.alali6784@gmail.com>
2023-07-25 19:02:13 +0400

    Added to do model and showed it on ContentView with list

---
+++ Todo App/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  Todo App
+//
+//  Created by Mohamed Jamal AlAli on 25/07/2023.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id = UUID()
+    var title : String
+    var isCompleted = false
+    
+}
